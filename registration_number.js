90ee09878a7ee1a006ef77df2461ae2c7b0985ea
azoladyantyi <aizydyantyi@gmail.com>
2017-03-30 11:37:25 +0200

    commit files with changes

--- registration_number.js
+++ registration_number.js
@@ -1,23 +1,45 @@
 var addRegNumber = document.querySelector('#addRegNumber');
 var regNumber = document.querySelector('#regNumber');
 var regN = document.querySelector('#regN');
+var options = document.querySelector('.options');
 
-function isValidReg(registratin) {
-  return registratin.startsWith('CA')
-  || registratin.startsWith('CJ')
-  || registratin.startsWith('CK');
+
+
+options.addEventListener('change', function(evt) {
+    var regList = document.querySelectorAll('li');
+
+    for (var i = 0; i < regList.length; i++) {
+        var registration = regList[i].innerHTML;
+
+        if (evt.target.value === "All") {
+            regList[i].style.display = "block";
+        } else if (registration.startsWith(evt.target.value)) {
+            regList[i].style.display = "block";
+        } else {
+            regList[i].style.display = "none";
+        }
+
+    }
+
+});
+
+
+function isValidReg(registration) {
+    return registration.startsWith('CA') ||
+        registration.startsWith('CJ') ||
+        registration.startsWith('CK');
 }
 
 regNumber.addEventListener('click', function() {
-  var enteredReg = addRegNumber.value;
-  if(!isValidReg(enteredReg))
-  //exit out this function
-  return;
+    var enteredReg = addRegNumber.value;
+    if (!isValidReg(enteredReg)) {
+        //exit out this function
+        return;
+    }
+    var li = document.createElement('li');
 
-  var li = document.createElement('li');
-  li.innerHTML = enteredReg;
-  regN.appendChild(li);
-
-
-
+    li.classList.add('bg-yellow');
+    li.innerHTML = enteredReg;
+    regN.appendChild(li);
 });
+//}
